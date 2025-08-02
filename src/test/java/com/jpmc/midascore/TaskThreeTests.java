24eb92667c25f9c84fe4f95f6f7fcdf7273fe59e
Subha <msubhadeep9635@gmail.com>
2025-08-02 20:16:41 +0530

    done all tasks

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.repository.UserRepository;
+
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
@@ -23,6 +26,9 @@ public class TaskThreeTests {
     @Autowired
     private FileLoader fileLoader;
 
+    @Autowired
+    private UserRepository userRepository;  
+
     @Test
     void task_three_verifier() throws InterruptedException {
         userPopulator.populate();
@@ -30,14 +36,18 @@ public class TaskThreeTests {
         for (String transactionLine : transactionLines) {
             kafkaProducer.send(transactionLine);
         }
-        Thread.sleep(2000);
+        Thread.sleep(2000);  // wait for processing
 
+       
+        UserRecord waldorf = userRepository.findByName("waldorf");
+        if (waldorf != null) {
+            System.out.println(">>> WALDORF FINAL BALANCE (rounded down): " + (int) waldorf.getBalance());
+        } else {
+            System.out.println(">>> WALDORF USER NOT FOUND");
+        }
 
-        logger.info("----------------------------------------------------------");
-        logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
         logger.info("use your debugger to find out what waldorf's balance is after all transactions are processed");
-        logger.info("kill this test once you find the answer");
         while (true) {
             Thread.sleep(20000);
             logger.info("...");
